f9518108f3f880afad2a5a0e4b5dbc5712b7724e
hannakortetjarvi <kortetjarvihanna@gmail.com>
2023-11-05 16:13:57 +0200

    Modify dockerfiles and add ui dockerfile

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+  },
   plugins: [
     vue(),
     vueJsx(),
