7c4276e01f1f789fdf7995632c9cafee250d7523
Hanna Kortetjärvi <kortetjarvihanna@gmail.com>
2023-11-07 20:13:23 +0200

    Selection specific user input

--- ui/src/components/InfoQuery.vue
+++ ui/src/components/InfoQuery.vue
@@ -1,42 +1,35 @@
 <template>
-    <p>Additional info needed</p>
-    <input v-model="input">
+    <p v-if="type !== 'deals'">Additional info needed</p>
+    <input v-model="input" v-if="type == 'sales'">
+    <div v-if="type == 'season'">
+        <VueDatePicker v-model="dateStart"></VueDatePicker>
+        <VueDatePicker v-model="dateEnd"></VueDatePicker>
+    </div>
 </template>
 
 <script>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 export default {
-  props: {
-    type: {
-      type: String,
-      required: true
-    }
-  },
-  data() {
-    return {
-      input: ''
-    }
-  },
-  watch: { 
-        input: function(newVal, oldVal) {
-          console.log('Input changed: ', oldVal, ' --> ', newVal)
+    components: { VueDatePicker },
+    props: {
+        type: {
+            type: String,
+            required: true
         }
-  },
-  computed: {
-    filteredDiscounts() {
-      switch (this.selected) {
-        case 'sales':
-          return this.discounts.filter((discount) => discount.type === 'sales');
-        case 'season':
-          return this.discounts.filter((discount) => discount.type === 'season');
-        case 'deals':
-          return this.discounts.filter((discount) => discount.type === 'deals');
-        default:
-          return [];
-      }
     },
-  },
-  mounted() {
-    // Fetch data
-  },
+    data() {
+        return {
+            input: '',
+            dateStart: null,
+            dateEnd: null
+        }
+    },
+    watch: { 
+        input: function(newVal, oldVal) {
+            console.log('Input changed: ', oldVal, ' --> ', newVal)
+        }
+    }
 };
 </script>
