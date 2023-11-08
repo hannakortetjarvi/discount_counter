7ac87710f5c33a01ea8cdf105a8c223dcc91e708
Hanna Kortetjärvi <kortetjarvihanna@gmail.com>
2023-11-08 14:40:10 +0200

    Create database tables

---
+++ api/database/migrations/2023_11_08_120519_create_discounts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('product_id');
+            $table->uuid('customer_id');
+            $table->string('type');
+            $table->decimal('amount', $precision = 10, $scale = 2);
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
