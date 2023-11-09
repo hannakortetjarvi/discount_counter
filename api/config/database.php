513973c464757fad66465458a40f0c0f151c2c5d
Hanna Kortetjärvi <kortetjarvihanna@gmail.com>
2023-11-09 17:57:23 +0200

    update ci

--- api/config/database.php
+++ api/config/database.php
@@ -66,9 +66,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-#            'host' => env('DB_HOST', '127.0.0.1'),
-#            'port' => env('DB_PORT', '5432'),
-#            'database' => env('DB_DATABASE', 'postgres'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'postgres'),
 #            'username' => env('DB_USERNAME', 'postgres'),
             'charset' => 'utf8',
             'prefix' => '',
