131899da72e720788633c7f7bc3b47e38dc92c9f
Hanna Kortetjärvi <kortetjarvihanna@gmail.com>
2023-11-09 18:26:12 +0200

    update ci

--- api/config/database.php
+++ api/config/database.php
@@ -70,7 +70,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'postgres'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', '${DB_PASSWORD}'),
+            'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
