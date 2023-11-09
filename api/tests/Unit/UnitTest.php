c4154b6580dbbddbe1601b775417850118fac28a
Hanna Kortetjärvi <kortetjarvihanna@gmail.com>
2023-11-09 16:09:25 +0200

    Add unit test

---
+++ api/tests/Unit/UnitTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Sale;
+use Tests\TestCase;
+
+class UnitTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_sale()
+    {
+        $sale = Sale::make([
+            'product_id' => '08e019cd-e39b-4db5-a7e1-59faaff37b05',
+            'customer_id' => '8e5efb6d-c129-4181-9f86-7a3f46be1780',
+            'count' => 5,
+        ]);
+
+        $sale->save();
+
+        $this->assertDatabaseHas('sales', [
+            'product_id' => '08e019cd-e39b-4db5-a7e1-59faaff37b05',
+            'customer_id' => '8e5efb6d-c129-4181-9f86-7a3f46be1780',
+            'count' => 5,
+        ]);
+    }
+}
